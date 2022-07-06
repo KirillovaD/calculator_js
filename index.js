12a862fbb6bc91cb0e355923c5d6d66dc169b8e8
Daria <100213152+KirillovaD@users.noreply.github.com>
2022-07-06 14:58:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function count(){
-  let countA = prompt('Введите первое чило', '');
+  let countA = +prompt('Введите первое чило', '');
   let countSign = prompt('+ , - , * , / ?', '');
-  let countB = prompt('Введите второе число?', '');
+  let countB = +prompt('Введите второе число?', '');
   let count;
   if (countSign === '+') {
-    count = +countA + +countB;
+    count = countA + countB;
   }
   else if(countSign === '-') {
-    count = +countA - +countB;
+    count = countA - countB;
   }
   else if(countSign === '/') {
-    count = +countA / +countB;
+    count = countA / countB;
   }
   else if(countSign === '*') {
-    count = +countA * +countB;
+    count = countA * countB;
   }
   else {
     alert('Я перегрелся. Попробуйте снова')
