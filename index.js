52b3420b49b3823e4bd7f839fe1ec67a1f6fa427
KirillovaD <sdaria@inbox.ru>
2022-07-06 19:19:28 +0300

    js add

--- index.js
+++ index.js
@@ -30,13 +30,5 @@ function multiply() {
   let result = Number(numA.value) * Number(numB.value)
   total.textContent = `= ${result}`;
 }
-//Изменение цвета
-function changeColor(){
-  let square = document.querySelectorAll(".change_clr")
-  square[0].style.backgroundColor = "darkmagenta";
-  square[1].style.backgroundColor = "darkmagenta";
-  square[2].style.backgroundColor = "darkmagenta";
-  square[3].style.backgroundColor = "darkmagenta";
-  };
 
 
