71b377987537093c884b8c594d03f18577bc6076
KirillovaD <sdaria@inbox.ru>
2022-07-09 14:07:11 +0300

    change js

--- div/index.js
+++ div/index.js
@@ -1,8 +1,5 @@
 //Изменение цвета
-function changeColor(){
-  let square = document.querySelectorAll(".change_clr")
-  square[0].style.backgroundColor = "darkmagenta";
-  square[1].style.backgroundColor = "darkmagenta";
-  square[2].style.backgroundColor = "darkmagenta";
-  square[3].style.backgroundColor = "darkmagenta";
+function changeColor(sender){
+  sender.classList.add('selected')
+  
   };
